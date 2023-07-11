d0f4f0c1f8357a8a05bba7625ce50f8b2a4e7827
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-07-11 14:15:26 +1000

    added connection to employee_db

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
 const cTable = require('console.table'); 
+
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db',
+    },
+    console.log('Connected to employee_db database.')
+);
